4bf444d8f4b1a9e61f5902d949416606401fa830
Diego G <d.g.g.87@hotmail.com>
2024-05-05 11:19:28 -0600

    added logic to hero section handleclick

--- src/components/ShoeCard.jsx
+++ src/components/ShoeCard.jsx
@@ -1,5 +1,33 @@
-const ShoeCard = () => {
-  return <div>ShoeCard</div>;
+const ShoeCard = ({imgURL, changeBigShoeImage, bigShoeImg}) => {
+  const handleClick = () => {
+    // console.log("bigShoeImg:", bigShoeImg);
+    // console.log("imgURL.thumbnail:", imgURL.thumbnail);
+    if (bigShoeImg !== imgURL.thumbnail) {
+      console.log('Changing big shoe image...');
+      changeBigShoeImage(imgURL.bigShoe);
+    }
+  };
+
+  return (
+    <div
+      className={`border-2 
+    rounded-xl
+    ${bigShoeImg === imgURL.bigShoe ? 'border-coral-red' : 'border-transparent'}
+    cursor-pointer max-sm:flex-1
+    `}
+      onClick={handleClick}
+    >
+      <div className="flex justify-center items-center bg-card bg-center bg-cover sm:w-40 sm:h-40 rounded-xl max-sm:p-4">
+        <img
+          src={imgURL.thumbnail}
+          alt="shoe collection"
+          width={127}
+          height={103}
+          className="object-contain"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default ShoeCard;
